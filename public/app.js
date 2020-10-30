c9a1ef4a0c580dbf78fff0ef000abd34bbae38c6
mgabotto <68821268+mgabotto@users.noreply.github.com>
2020-10-29 22:04:57 -0300

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,26 @@
+// function selection () {
+    
+// }
+
+
+
+// var randNum;
+// var posicionesVacias = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// var posiciones = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// function click() {
+
+//   randNum = Math.floor(Math.random() * posicionesVacias.length);
+//   posicionesVacias.splice(randNum, 1);
+//   posiciones.splice(randNum, 1, "X");
+
+//   console.log(randNum + "  randNum");
+//   console.log(posicionesVacias.length + "  posiciones vacias length");
+//   console.log(posiciones + "  posiciones");
+//   console.log(posicionesVacias + "  posiciones vacias");
+
+// }
+
+
+
+console.log(firebase);
